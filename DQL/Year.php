af2589e8d8e0ef4b238cad15ce3de813ce16d93a
Karous Nabil <n.karous@esystema.fr>
2013-07-17 19:41:44 +0100

    add DQL Postgres

---
+++ DQL/Year.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Xeonys\Bundle\BanksBundle\DQL;
+
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+class Year extends FunctionNode
+{
+    public $date;
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return sprintf('EXTRACT(YEAR FROM(%s))', $this->date->dispatch($sqlWalker));
+    }
+
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->date = $parser->ArithmeticPrimary();
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
